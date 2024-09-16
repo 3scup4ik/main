0e97528315edf69f2004fe61ee406fbe6b8f4c5a
3scup4ik <glebsavvateev1970@gmail.com>
2024-09-16 21:29:39 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+
+from math import *
+r=0
+a=float(input())
+d=float(input())
+if d>0 and 1 >= (cos(log(d, e) - 31 * 10 ** (-3))) >= (-1) and (2-pi+log(d,e)*(cos(log(d,e)-31*10**(-3))))!=0 and (a*log(d,e))/(2-pi+log(d,e)*(cos(log(d,e)-31*10**(-3))))>0:
+    r=sqrt((a*log(d,e))/(2-pi+log(d,e)*(cos(log(d,e)-31*10**(-3)))))
+    print(r)
+else:
+    print("Вычисление невозможно")
+
+'''
+from math import *
+r=0
+a = input()
+d = input()
+p=a+d
+if p.isalpha():
+    print("Вычисление невозможно")
+else:
+    a=float(a)
+    d=float(d)
+    if d>0 and 1 >= (cos(log(d, e) - 31 * 10 ** (-3))) >= (-1) and (2-pi+log(d,e)*(cos(log(d,e)-31*10**(-3))))!=0 and (a*log(d,e))/(2-pi+log(d,e)*(cos(log(d,e)-31*10**(-3))))>0:
+        r=sqrt((a*log(d,e))/(2-pi+log(d,e)*(cos(log(d,e)-31*10**(-3)))))
+        print(r)
+    else:
+        print("Вычисление невозможно")
+'''
